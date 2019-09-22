ed0284abba059cce51e4060da5fa8aa75fb72a69
Rendell Lasola <rendelllasola@gmail.com>
2019-09-23 04:12:21 +0800

    tests for actions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./component/header";
 import Headline from "./component/headline";
+import SharedButton from "./component/button";
+import ListItem from "./component/listitem";
+import { connect } from "react-redux";
+import { fetchPosts } from "./actions";
 import "./app.scss";
 
 const tempArr = [
@@ -13,19 +17,59 @@ const tempArr = [
   }
 ];
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <section className="main">
-        <Headline
-          header="Posts"
-          desc="Click the button to render posts."
-          tempArr={tempArr}
-        />
-      </section>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.fetch = this.fetch.bind(this);
+  }
+
+  fetch() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    const { posts } = this.props;
+
+    const configButton = {
+      buttonText: "Get posts",
+      emitEvent: this.fetch
+    };
+
+    return (
+      <div className="App">
+        <Header />
+        <section className="main">
+          <Headline
+            header="Posts"
+            desc="Click the button to render posts."
+            tempArr={tempArr}
+          />
+          <SharedButton {...configButton} />
+          {posts.length > 0 && (
+            <div>
+              {posts.map((post, index) => {
+                const { title, body } = post;
+                const configListItem = {
+                  title,
+                  desc: body
+                };
+                return <ListItem key={index} {...configListItem} />;
+              })}
+            </div>
+          )}
+        </section>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(App);
