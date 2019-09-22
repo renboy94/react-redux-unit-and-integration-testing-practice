2378720880902829e9d24cfae4c592ff2c421a68
Rendell Lasola <rendelllasola@gmail.com>
2019-09-22 22:39:04 +0800

    Adding PropTypes to the headline component

--- src/component/headline/headline.spec.js
+++ src/component/headline/headline.spec.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import Headline from "./index";
 
-import { findByTestAttr } from "./../../../utils";
+import { findByTestAttr, checkProps } from "./../../../utils";
 
 const setUp = (props = {}) => {
   const component = shallow(<Headline {...props} />);
@@ -10,6 +10,27 @@ const setUp = (props = {}) => {
 };
 
 describe("Headline Component", () => {
+  describe("Checking PropTypes", () => {
+    it("Should not throw a warning", () => {
+      const expectedProps = {
+        header: "Test Header",
+        desc: "Test Desc",
+        tempArr: [
+          {
+            fName: "Test fName",
+            lName: "Test fName",
+            email: "test@email.com",
+            age: 23,
+            onlineStatus: false
+          }
+        ]
+      };
+
+      const propsErr = checkProps(Headline, expectedProps);
+      expect(propsErr).toBeUndefined();
+    });
+  });
+
   describe("Have props", () => {
     let wrapper;
     beforeEach(() => {
